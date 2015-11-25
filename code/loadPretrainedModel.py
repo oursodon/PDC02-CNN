a4ca41d003193e421d21b690c4689645b1b6be61
arinik9 <arinik9@hotmail.com>
2015-11-25 13:32:43 +0100

    fixing processImage method

--- code/loadPretrainedModel.py
+++ code/loadPretrainedModel.py
@@ -8,96 +8,182 @@
 import caffe
 import numpy as np
 from skimage.transform import pyramid_gaussian
+import skimage
+import Image
 import math
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-def getImagePyramid(image,downScale):
-    dim, cols, rows = image.shape
-    maxLayer = min(math.floor(math.log(rows//36)//math.log(downScale)),math.floor(math.log(cols//36)//math.log(downScale)))
-    return tuple(pyramid_gaussian(image,max_layer=maxLayer, downscale=downScale))
-
-def setBoxCorners(left_corner, top_corner, window_length):
-    box=(left_corner,top_corner,left_corner+window_length,\
-            top_corner+window_length)
-    return box
+def getImagePyramid(image,downScale,window_length):
+    # Nejat changed pyramid method because he needs an image instance in processImage() method
+    # for croppping instead of ndarray computed by Quentin's method (pyramid_gaussian)
+    w, h = image.size
+    # maxLayer = min(math.floor(math.log(rows//36)//math.log(downScale)),\
+    #   math.floor(math.log(cols//36)//math.log(downScale)))
+    # return tuple(pyramid_gaussian(image,max_layer=maxLayer, downscale=downScale))
+    imageList = [image]
+    print "For original image:  => size: "+str(image.size)
+    counter=1
+    while True:
+        if w>window_length and h>window_length:
+            w = int(w * downScale)
+            h = int(h * downScale)
+            #Image.BICUBIC is one of the methods for resizing => I think it is best
+            im = image.resize((w, h), Image.BICUBIC)
+            #we can save this intermediate image => im.save("filename.jpeg")
+            print "For intermediate image: "+str(counter)+" => size: "+str(im.size)
+            imageList.append(im)
+        else:
+            break
+        counter+=1
+    return imageList
+#end of the method
 
 def findCentrumPoint(left, top, window_length):
-    x = (left+window_length)/2
-    y = (top+window_length)/2
+    x = left+(window_length/2)
+    y = top+(window_length/2)
     return (x, y)
+#end of the method
 
-def fusionMatrixInOriginalScale(window_length, matrix_list):
-    matrix_final = np.zeros((window_length, window_length))
-    for matrix in matrix_list:
-        print "TODO"
-        # EGER MATRIX BOYUTU 36dan buyukse 36 cekmek lazim
-    return matrix_final
+def processImage(imageList,downScale,network,threshold,window_length,step):
+    ### INPUT ###
+    # ImageList: An image pyramid of given image
+    # downScale: subsampling degree => we reduce the image size according to downScale
+    # network: Caffe Convolutional Neurol Network Pretrained Model
+    # threshold: with this threshold, we accept if there is face in window zone
+    # window_length: the size of slicing window => square window
+    # step: we slice our window with this step => step=1 means we look at each pixel
 
-def processImage(imageList,scale,network):
-#TODO Save face size
-    window_length=36 # original scale => (36, 36)
+    ### OUTPUT ###
+    # facesPosition: sorted and unique position list
+
+    #TODO Save face size
+
     left_corner=0
     top_corner=0
-
     facesPosition = []
-    truc = 0
+    scale_degree= 0  # truc = 0
 
-    for image in imageList:
-        dim, width, height = image.shape
-      
-        for left in xrange(0,width-window_length+1,10):
+    #a log file is good to follow each step
+    with open('traces.log', 'w') as f:
+        for image in imageList:
+            width, height = image.size
+            f.write("Image : " + str(scale_degree)+"\n")
+            for left in xrange(0, width-window_length+1, step):
+                for top in xrange(0, height-window_length+1, step):
+                    cropped = image.crop((left, top, left+window_length,\
+                            top+window_length))
+                    cropped.load()
+                    # we need to swap image dimensions for image to be loaded => transpose()
+                    # because the dimensions of the image to be loaded is  (36, 36, 1).
+                    # That is why we will use (2,1,0) order
+                    # w.r.t our network expect theses dimensions: (1, 1, 36, 36)
+                    # we can verify it by typing: net.blobs['data'].data.shape or facenet_deploy.prototxt
+                    data = np.asarray(cropped)
+                    img = data[:, :, np.newaxis]
+                    img = skimage.img_as_float(img).astype(np.float32)
+                    img = img.transpose((2,0,1))
+                    #cropped = image[:,left:left+window_length,top:top +window_length]
+                    network.blobs['data'].data[...] = img
+                    out = network.forward()
+                    # out['prob'][0] gives us the score tuple of non-face class 0 and face class 1
+                    #so we are interested in out['prob'][0][1] for the score of class 1
+                    f.write("left: "+str(left)+" top: "+str(top)+" => score: "+\
+                            str(out['prob'][0][1])+" => image size: ["\
+                            +str(image.size)+"]\n")
 
-            for top in xrange(0,height-window_length+1,10):
-                #cropped = image.crop(setBoxCorners(left, top, window_length))
-                cropped = image[:,left:left+window_length,top:top +window_length]
-                #print cropped.shape
-                network.blobs['data'].data[...] = cropped
-                out = network.forward()
-                result = out['prob'][0].argmax() # it is a face image #TODO change the line
-                if result == 1:
-                    #print "left:", left, "top:", top
-                    x, y = findCentrumPoint(left, top, window_length)
-                    #print x, y
-                    facesPosition.append ( (x * (scale** truc) ,y *( scale ** truc ) ))
+                    if out['prob'][0][1]>threshold:
+                        x, y = findCentrumPoint(left, top, window_length)
+                        scaled_centrum_x = int(x*(1/float(downScale) ** scale_degree)) # truc
+                        scaled_centrum_y = int(y*(1/float(downScale) ** scale_degree)) # truc
+                        f.write("Good! => centrum_x: "+str(x)+" centrum_y: "\
+                                +str(y)+" new_x: "+str(scaled_centrum_x)+" new_y: "\
+                                +str(scaled_centrum_y)+"\n")
+                        # uniqueness of position list
+                        if (scaled_centrum_x, scaled_centrum_y) not in facesPosition:
+                            facesPosition.append((scaled_centrum_x, scaled_centrum_y))
+                        #example: 50/0.8=40 => 40*(1/0.8)=50
+            scale_degree += 1 # truc += 1
 
-        truc += 1
-        print "Image : " + str(truc)
-
-    return facesPosition
+    return sorted(facesPosition) # sorted by x
+#end of the method
 
 
-caffe.set_mode_cpu()
-net = caffe.Net("../facenet_deploy.prototxt", "../facenet_iter_200000.caffemodel", caffe.TEST)
 
-# we need to swap image dimensions for image to be loaded => transpose()
-# because the dimensions of the image to be loaded is  (36, 36, 1). That is why we will use (2,1,0) order
-# w.r.t our network expect theses dimensions: (1, 1, 36, 36)
-# we can verify it by typing: net.blobs['data'].data.shape or facenet_deploy.prototxt
-image =  caffe.io.load_image("../imgTest.jpg", False).transpose((2,1,0))
+##############
+#### MAIN ####
+##############
 
-imageList = getImagePyramid(image,1.2)
+if __name__ == '__main__':
 
-print image.shape
-print image
-print len(imageList)
+    ##################################
+    #### PARAMETER CONFIGURATIONS ####
+    ##################################
 
-positions = processImage(imageList,1.2,net)
-print positions
-implot = plt.imshow(image[0],cmap = cm.Greys_r)
-plt.scatter([pos[0] for pos in positions],[pos[1] for pos in positions])
-plt.show()
-"""
-net.blobs['data'].data[...] = image
-out = net.forward()
-print("For the image: 0/image01_36x36.pgm")
-print("Predicted class is #{}.".format(out['prob'][0].argmax()))
-#output is 0. So it is not a face image!
+    downScale = 0.8
+    threshold = 0.87 #for accepting if an image is a face-image or not
+    window_length = 36
+    step = 1 # for slicing window
+    image_dir = "../multiple_face/"
+    filename = "Japan.jpg" # another example: "ucsd.png"
+    filepath = image_dir + filename
+    caffemodel = "../facenet_iter_200000.caffemodel"
+    model = "../facenet_deploy.prototxt"
 
-image2 = caffe.io.load_image("../1/image11_36x36.pgm" , False).transpose((2,1,0))
-net.blobs['data'].data[...] = image2
-out = net.forward()
-print("For the image: 1/image11_36x36.pgm")
-print("Predicted class is #{}.".format(out['prob'][0].argmax()))
-#output is 1. So it is a face image!
-"""
+    ###############
+    #### CAFFE ####
+    ###############
+
+    #set cpu mod
+    caffe.set_mode_cpu()
+    #load model (deploy) and weights (caffemodel)
+    net = caffe.Net(model, caffemodel, caffe.TEST)
+
+    #########################################
+    #### LOADING IMAGE AND PREPROCESSING ####
+    #########################################
+
+    image = Image.open(filepath)
+    #image = Image.open("../ucsd.png")
+    data = np.asarray(image) # image in a matrix format
+
+    #get image pyramid
+    imageList = getImagePyramid(image,downScale,window_length)
+    print "There are "+str(len(imageList))+" images in the pyramid (with original image)"
+
+
+    ####################
+    #### PROCESSING ####
+    ####################
+
+    positions = processImage(imageList,downScale,net,threshold,window_length,step)
+
+    print  positions
+    print "In total, there are "+str(len(positions))+" unique positions"
+
+    ########################
+    #### POSTPROCESSING ####
+    ########################
+
+    ### STEP 1 ###
+
+    # TODO clustering algorithm for finding the centroid of each dense group in positions
+    # We will plot this new centroids in plot actually
+    # We may do that by an 'Connected Component' algorithm
+    # In that algorithm, the decision of creating a new group may be done with euclidian distance or weights
+
+    ### STEP 2 ###
+
+    # generating local pyramid based on each centroid found in clustring algorithm
+    # and execute pretrained model again in the local pyramid (for each sub-image)
+    # gathering scores of each local pyramid
+    # deciding if there is a face or not in each local
+
+    ##############
+    #### PLOT ####
+    ##############
+
+    # Scatter plot for showing each postion on the given image
+    implot = plt.imshow(data,cmap = cm.Greys_r)
+    plt.scatter([pos[0] for pos in positions],[pos[1] for pos in positions])
+    plt.show()
